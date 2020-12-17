5fd4e6fbb49e852ec9016f09875bf370d81c19fd
FRZY03 <75489871+FRZY03@users.noreply.github.com>
2020-12-17 11:36:27 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,19 +101,45 @@ TW_USE_TOOLBOX := true
 TW_OZIP_DECRYPT_KEY := "ACAC1E13A72531AE4A1B22BB31C1CC22"
 
 
-#SHRP_Variables
+# Path of your SHRP Tree
 SHRP_PATH := device/realme/RMX1821
+# Maintainer name *
 SHRP_MAINTAINER := FREEZY
+# Device codename *
 SHRP_DEVICE_CODE := RMX1821
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Treble
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A/B
+# SHRP Padding Flag (Only for rounded corner devices.) [Optional]
+# You have to change these values according to your device's roundness.
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+# For Notch devices [Optional]
+SHRP_NOTCH := true
+# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock [Optional]
+SHRP_EXPRESS := true
+# SHRP Dark mode, use this flag to have dark theme set by default [Optional]
+SHRP_DARK := true
+# put this 0 if device has no EDL mode *
 SHRP_EDL_MODE := 1
+# Put your device's paths from fstab *
 SHRP_EXTERNAL := /external_sd
 SHRP_INTERNAL := /sdcard
-SHRP_OTG := /usbotg
-SHRP_REC := /dev/block/bootdevice/by-name/recovery
-SHRP_REC_TYPE := Treble
-SHRP_DEVICE_TYPE := A/B
+SHRP_OTG := /usb_otg
+# Put 0 to disable flashlight *
 SHRP_FLASH := 1
-#SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
-#SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
-#SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
-#SHRP_FLASH_MAX_BRIGHTNESS := 200
+# These are led paths, find yours then put here [Optional]
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+# Max Brightness of LED [Optional]
+SHRP_FLASH_MAX_BRIGHTNESS := 200
+# Check your device's recovery path, dont use blindly *
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+# Use this flag only if your device is A/B *
+SHRP_AB := true
+# Force mount system in /system despite SAR policy, useful for maintaining backwards compatibility and/or Samsung devices. [Optional]
+SHRP_NO_SAR_AUTOMOUNT := true
+
